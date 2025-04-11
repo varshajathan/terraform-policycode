5b8b8fe6b7908b5b3d7fac34daaa35ee212858a5
varshajathan <48399281+varshajathan@users.noreply.github.com>
2025-04-11 15:12:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 
-variable "gcp_credentials" {
-  description = "Base64-encoded GCP service account key"
-  type        = string
-}
+//variable "gcp_credentials" {
+ // description = "Base64-encoded GCP service account key"
+  //type        = string
+//}
 
 variable "project_id" {
   description = "GCP project ID"
@@ -14,13 +14,4 @@ variable "region" {
   type        = string
 }
 
-variable "machine_type" {
-  description = "GCP machine type"
-  type        = string
-  default     = "e2-medium"
-}
 
-variable "names" {
-  description = "name of the vm instances"
-  type        = string
-}
